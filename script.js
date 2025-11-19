369e247ead5cab57efc1f6958f38c911ce05bd69
ADARSH2103-byte <chaudharyadarsh29@gmail.com>
2025-11-19 17:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,33 @@
 function Person(name, age) {}
 
 function Employee(name, age, jobTitle) {}
+// Person Class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // Method to greet
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+// Employee Class
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // calling parent (Person) constructor
+    this.jobTitle = jobTitle;
+  }
+
+  // Job-specific greeting
+  jobGreet() {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  }
+}
 
 // Do not change code below this line
 window.Person = Person;
